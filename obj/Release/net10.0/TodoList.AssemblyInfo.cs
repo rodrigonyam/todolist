45de056054157dbb5221196e85a29bc8a8233ad5
Rodrigue Nyamwoga <rodrigonyam@gmail.com>
2026-03-13 01:47:12 -0400

    newr commits

--- obj/Release/net10.0/TodoList.AssemblyInfo.cs
+++ obj/Release/net10.0/TodoList.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+197f67e40e9ea8aae604cfc8728f2328a8a83f5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
